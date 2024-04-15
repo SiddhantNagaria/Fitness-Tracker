b38c962a54943238c21de8dd7d75814ef17adc74
Siddhant Nagaria <siddhant.nagaria1234@gmail.com>
2024-04-15 09:14:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2> Namaste! Welcome to Our website Fifty Fitness </h2>
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+    FirstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    LastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    Email :{
+        type : String,
+        required: true,
+    },
+   
+});
+
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema); 
